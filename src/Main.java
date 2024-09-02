f9400d0813b92cec98e28dff769d8eed9b973fde
Na <trv.nag@yandex.ru>
2024-09-02 18:17:54 +0800

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 13676;
 
-                int bonus = ticket / 20;
-                System.out.println("Бонусы =" + bonus );
+        int bonus = ticket / 20;
+        System.out.println("Бонусы =" + bonus);
     }
 }
